1fe32552f55aa7c182e36221225394c66c659703
taeseong14 <kwontaeseong14@gmail.com>
2022-06-09 10:33:42 -0500

    프론트 루트 설정

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./back');
 const app = express();
 
-app.use(express.static('front'));
+app.use(express.static(__dirname + '/front'));
 app.use(router);
 
 app.listen(3000, () => console.log('listening on port 3000'));
